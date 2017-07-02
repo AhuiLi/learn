4577da205f69430acc60bbd2c07262ff783c1225
ahui <li_ahui@foxmail.com>
2017-07-02 17:04:48 +0800

    test module require

--- python/hello.py
+++ python/hello.py
@@ -14,10 +14,18 @@ elif 0:
 d = {"name":"ahui","lang":[1,2,3]}
 print(d['name'])
 print(d.get('lang','nidaye'))
-'''
+
 t = (1,2,3,3)
 s = set((1,2,3,3))
 #s1 = set((1,[2,3]))
 print(s)
 print(t)
+'''
 #print(s)
+
+# 安装第三方模块，测试使用图像处理库
+from PIL import Image
+im = Image.open('test.jpg')
+print(im.format, im.size, im.mode)
+im.thumbnail((200,100))
+im.save('test_thumb.jpg', 'JPEG')
